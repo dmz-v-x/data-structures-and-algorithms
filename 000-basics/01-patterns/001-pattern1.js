a5f7dde5f31e036841c1097613f3c60d7773c1e1
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-01-19 13:51:02 +0530

    Create 001-pattern1.js
    Implement a function to print a star pattern.

---
+++ 000-basics/01-patterns/001-pattern1.js
@@ -0,0 +1,15 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+const number = 5;
+
+function pattern(number) {
+  for (let i = 0; i < number; i++) {
+      console.log("* * * * *");
+  }
+}
+
+pattern(number);
