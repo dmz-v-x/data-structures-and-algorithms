ea01a8dfadd1552906eea5847942582fc1c941ed
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-13 21:14:33 +0530

    Update 010-move-zeros-to-end.js

--- 001-arrays/code/010-move-zeros-to-end.js
+++ 001-arrays/code/010-move-zeros-to-end.js
@@ -1,5 +1,16 @@
 // Naive Solution:
 
+// Approach 1:
+
+// 1. Create a new array.
+// 2. Iterate through the array and whenever you find a non zero element push it to created array.
+// 3. Number of zeros = length of original array - length of new array.
+// 4. Replace elements from new array to original array one by one.
+// 5. loop through the length of number of zeros and add the zeros starting from newArray.length to orignalArray.length -1;
+
+
+// Approach 2:
+
 // Time Complexity: O(n^2) // [0,0,0,0,0,1]
 // Space Complexity: O(1)
 
