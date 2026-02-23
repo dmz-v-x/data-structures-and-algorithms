60dc92c48a51a0030830f32ba95f036e5d09131e
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-23 14:45:32 +0530

    Create 001-binary-tree-representation.js

---
+++ 014-binary-tree/code/001-binary-tree-representation.js
@@ -0,0 +1,30 @@
+// 1. A tree node class
+
+class TreeNode {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// 2. Creating Nodes
+
+const nodeA = new TreeNode(10); // nodeA → { value: 10, left: null, right: null }
+const nodeB = new TreeNode(20); // nodeB → { value: 20, left: null, right: null }
+const nodeC = new TreeNode(30); // nodeC → { value: 30, left: null, right: null }
+
+// 3. Connecting Nodes -> Manual Tree Construction
+
+const root = new TreeNode(10);
+const leftChild = new TreeNode(20);
+const rightChild = new TreeNode(30);
+
+root.left = leftChild;
+root.right = rightChild;
+
+// Visual structure:
+
+//         10
+//        /  \
+//       5   15
