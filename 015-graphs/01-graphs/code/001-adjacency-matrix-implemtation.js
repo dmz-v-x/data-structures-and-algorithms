d652eaa30278ac4a52bc0afe6ba2c421353b063e
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-27 12:26:47 +0530

    Update 001-adjacency-matrix-implemtation.js

--- 015-graphs/01-graphs/code/001-adjacency-matrix-implemtation.js
+++ 015-graphs/01-graphs/code/001-adjacency-matrix-implemtation.js
@@ -50,4 +50,65 @@ class Graph {
     this.matrix[i][j] = 0;
     this.matrix[j][i] = 0;
   }
+
+  // 5. Removing a Vertex
+
+  removeVertex(vertex){
+    const index = this.getIndex(vertex);
+    if(index === -1) return;
+
+    // Remove vertex
+    this.vertices.splice(index, 1);
+
+    // Remove row
+    this.matrix.splice(index, 1);
+
+    // Remove column
+    for(let row of this.matrix){
+      row.splice(index, 1);
+    }
+  }
+
+  // 6. Adding and Edge (Directed Graph)
+
+  addEdge(v1, v2){
+    const i = this.getIndex(v1);
+    const j = this.getIndex(v2);
+
+    if(i === -1 || j === -1) return;
+
+    this.matrix[i][j] = 1;
+  }
+
+  // 7. Adding an Edge (Weighted Graph)
+
+  addEdge(v1, v2, weight = 1){
+    const i = this.getIndex(v1);
+    const j = this.getIndex(v2);
+
+    if(i === -1 || j === -1) return;
+
+    this.matrix[i][j] = weight;
+    this.matrix[j][i] = weight; // remove for directed
+  }
+
+  // 8. Check if edge exists
+
+  hasEdge(v1, v2){
+    const i = this.getEdge(v1);
+    const j = this.getEdge(v2);
+
+    if(i === -1 || j === -1) return false;
+
+    return this.matrix[i][j] !== 0;
+  }
+
+  // 9. Printing the Graph
+
+  print(){
+    console.log("Vertices:", this.vertices);
+    console.log("Matrix:");
+    console.table(this.matrix);
+  }
+  
 }
