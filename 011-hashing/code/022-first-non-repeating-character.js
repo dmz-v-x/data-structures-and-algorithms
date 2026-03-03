086deab76c0f1bf79da0b178c6ce753dc264ca02
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-03 14:08:45 +0530

    Update 022-first-non-repeating-character.js

--- 011-hashing/code/022-first-non-repeating-character.js
+++ 011-hashing/code/022-first-non-repeating-character.js
@@ -42,9 +42,72 @@ function firstNonRepeatingChar(str){
 }
 
 
+// Better Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+function firstNonRepeatingChar(str){
+  const freq = {};
 
+  // count frequency
+  for(let char of str){
+    freq[char] = (freq[char] || 0) + 1;
+  }
 
+  // Find first non-repeating char
+  for(let char of str){
+    if(freq[char] === 1){
+      return char;
+    }
+  }
+  return null;
+}
+
+// Optimal Approach
+// Time Complexity: O(n)
+// Space Compliexity: O(1)
+
+function firstNonRepeatingChar(str){
+  const freqMap = new Map();
+
+  // First pass: store frequency
+  for(let char of str){
+    freqMap.set(char, (freqMap.get(char) || 0) + 1);
+  }
+
+  // Second pass: find first unique char
+  for(let char of str){
+    if(freqMap.get(char) === 1){
+      retrun char;
+    }
+  }
+  return null;
+}
+
+// Optimal Appraoch (Only for lowercase english letters)
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+function firstNonRepeatingChar(str){
+  let freq = new Array(26).fill(0);
+
+  // Count Frequency
+  for(let i = 0; i<str.length; i++){
+    const index = str.charCodeAt(i) - 97;
+    freq[index]++;
+  }
+
+  // Find first occurrence
+
+  for(let i = 0; i<str.length; i++){
+    const index = str.charCodeAt(i) - 97;
+    if(freq[index] === 1){
+      return str[i];
+    }
+  }
+  return null;
+}
 
 
 
