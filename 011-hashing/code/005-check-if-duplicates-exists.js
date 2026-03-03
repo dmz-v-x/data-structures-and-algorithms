058eb7b35374acaa90355d28dd102d58e1f34cc7
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-03 16:13:37 +0530

    Update 005-check-if-duplicates-exists.js

--- 011-hashing/code/005-check-if-duplicates-exists.js
+++ 011-hashing/code/005-check-if-duplicates-exists.js
@@ -4,7 +4,21 @@
 
 // Output:
 
-//     true
+//     true 
+
+// Naive Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+function checkDuplicatesExists(arr){
+  for(let i = 0; i<arr.length; i++){
+    for(let j = i + 1; j<arr.length; j++){
+      if(arr[i] === arr[j]){
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 function checkDuplicatesExists(arr){
   const seen = new Set();
