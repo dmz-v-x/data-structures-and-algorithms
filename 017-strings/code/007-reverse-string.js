db43c007655e8a56e113f6285fce4efbfbdc24a0
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-04 13:13:54 +0530

    Update 007-reverse-string.js

--- 017-strings/code/007-reverse-string.js
+++ 017-strings/code/007-reverse-string.js
@@ -19,3 +19,36 @@ function reverseString(str){
   return str.split('').sort().join('');
 }
 
+// Better Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function reverseString(str){
+  let result = '';
+  for(let i = str.length - 1; i>=0; i--){
+    result += str[i];
+  }
+  return result;
+}
+
+// Optimal Approach
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+function reverseString(str){
+  let arr = str.split('');
+  let left = 0;
+  let right = arr.length-1;
+  
+  while(left < right){
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    left++;
+    right--;
+  }
+  return arr.join('');
+}
+
+// We cannot reverse a string using constant space we have to use an array becuase JavaScript strings are immutable
