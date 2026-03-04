791a5ce270cb98f4cc9a3d60638085f55dc12fa7
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-04 12:15:25 +0530

    Create 006-group-anagrams.js

---
+++ 017-strings/code/006-group-anagrams.js
@@ -0,0 +1,14 @@
+// Given an array of strings, group the strings that are anagrams of each other.
+
+// Two strings are anagrams if they contain the same characters with the same frequencies, just in a different order.
+
+// Example:
+
+// Input: ["eat","tea","tan","ate","nat","bat"]
+
+// Output:
+// [
+//   ["eat","tea","ate"],
+//   ["tan","nat"],
+//   ["bat"]
+// ]
