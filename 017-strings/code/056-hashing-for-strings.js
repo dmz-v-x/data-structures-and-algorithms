c9b26a27d636c3f555d1f64a486992406688d89e
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-08 10:16:16 +0530

    Create 056-hashing-for-strings.js

---
+++ 017-strings/code/056-hashing-for-strings.js
@@ -0,0 +1,13 @@
+// Hashing for Strings
+
+// String hashing basics
+
+// Why hashing is needed
+
+// Polynomial rolling hash
+
+// Rabin-Karp algorithm
+
+// Avoiding collisions
+
+// Double hashing
