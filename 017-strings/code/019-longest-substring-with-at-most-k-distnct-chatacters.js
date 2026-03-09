3648f8cb709cd6572b1d6afa16facf6dc34a860c
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-09 15:21:29 +0530

    Update 019-longest-substring-with-at-most-k-distnct-chatacters.js

--- 017-strings/code/019-longest-substring-with-at-most-k-distnct-chatacters.js
+++ 017-strings/code/019-longest-substring-with-at-most-k-distnct-chatacters.js
@@ -1,1 +1,46 @@
+// Given a string s and an integer k, return the length of the longest substring that contains at most k distinct characters.
 
+// Example 1
+
+// Input
+
+// s = "eceba"
+// k = 2
+
+// Substrings
+
+// ec
+// ece
+// ce
+// eb
+// ba
+
+// Longest valid substring
+
+// ece
+
+// Output
+
+// 3
+// Example 2
+
+// Input
+
+// s = "aa"
+// k = 1
+
+// Longest substring
+
+// aa
+
+// Output
+
+// 2
+
+
+// Naive Approach
+
+function longestSubstringKDistinct(s, k){
+  let maxLen = 0;
+  for(let i = 0; )
+}
