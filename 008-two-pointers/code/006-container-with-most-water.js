de327643387666347060371dfe3ff614e32dde36
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-10 12:13:33 +0530

    Update 006-container-with-most-water.js

--- 008-two-pointers/code/006-container-with-most-water.js
+++ 008-two-pointers/code/006-container-with-most-water.js
@@ -32,3 +32,27 @@ function maxArea(height){
   }
   return maxArea;
 }
+
+
+// Better Solution
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+function maxArea(height){
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while(left < right){
+    let width = right - left;
+    let h = Math.min(height[left], height[right]);
+    let area = width * h;
+
+    maxArea = Math.max(maxArea, area);
+    if(height[left] < height[right]){
+      left++;
+    }else{
+      right--;
+    }
+  }
+  return maxArea;
+}
