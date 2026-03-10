e9a3df40125fa4adad0232a1a0e2e5a96480923b
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-10 14:11:13 +0530

    Update 001-maximum-subarray-sum-of-size-k.js

--- 009-sliding-window/code/001-maximum-subarray-sum-of-size-k.js
+++ 009-sliding-window/code/001-maximum-subarray-sum-of-size-k.js
@@ -24,6 +24,7 @@
 
 // Naive Approach:
 // Time Complexity: O(N * K)
+// Space Complexity: O(N)
 function maxSubarraySum(arr, k){
   let maxSum = -Infinity
   for(let i = 0; i<=arr.length - k; i++){
@@ -34,4 +35,43 @@ function maxSubarraySum(arr, k){
     maxSum = Math.max(maxSum, sum)
   }
   return maxSum;
+}
+
+// Better Approach: Using Prefix sum
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+function maxSubarraySum(arr, k){
+  let n = arr.length;
+  let prefix = new Array(n);
+  prefix[0] = arr[0];
+  
+  for(let i = 1; i<n; i++){
+    prefix[i] = prefix[i-1]+arr[i];
+  }
+
+  let maxSum = -Infinity;
+
+  for(let i = 0; i<n; i++){
+    let j = i+k-1;
+    let sum = prefix[j] - (i > 0 ? prefix[i-1] : 0);
+    maxSum = Math.max(maxSum, sum);
+  }
+  return maxSum;
+}
+
+// Sliding Window
+// Total Complexity: O(N)
+// Space Complexity: O(1)
+function maxSubarraySum(arr, k){
+  let windowSum = 0;
+  for(let i = 0; i<k; i++){
+    windowSum += arr[i];
+  }
+
+  let maxSum = windowSum 
+  for(let i = k; i<arr.length; i++){
+    windowSum = wimdowSum - arr[i-k] + arr[i];
+    maxSum = Math.max(windowSum, maxSum);
+  }
+  return maxSum
 }
