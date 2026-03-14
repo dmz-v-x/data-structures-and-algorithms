945a05ef649435af8b5b45f6f96544c728ca3822
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-14 19:56:44 +0530

    Update 039-counting-prefix-frequency.js

--- 017-strings/code/039-counting-prefix-frequency.js
+++ 017-strings/code/039-counting-prefix-frequency.js
@@ -37,4 +37,13 @@ function countSubarry(arr, k){
   let prefixSum = 0;
   let count = 0;
   let map = new Map();
+  map.set(0, 1);
+  for(let num of nums){
+    prefixSum += num;
+    if(map.has(prefixSum - k)){
+      count += map.get(prefixSum - k);
+    }
+    map.set(prefixSum, (map.get(prefixSum) || 0) + 1);
+  }
+  return count;
 }
