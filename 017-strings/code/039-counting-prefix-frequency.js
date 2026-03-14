dde23213ff9d7e53336821759fbdbbe7d144a9e0
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-14 08:37:44 +0530

    Update 039-counting-prefix-frequency.js

--- 017-strings/code/039-counting-prefix-frequency.js
+++ 017-strings/code/039-counting-prefix-frequency.js
@@ -1,1 +1,40 @@
+// Given an array and integer k, count number of subarrays whose sum = k.
 
+// Example:
+
+// arr = [1,1,1]
+// k = 2
+
+// Subarrays with sum = 2
+
+// [1,1] (index 0-1)
+// [1,1] (index 1-2)
+
+// Answer
+
+// 2
+
+// Naive Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+function countSubarray(arr, k){
+  let count = 0;
+  for(let i = 0; i<arr.length; i++){
+    let sum = 0;
+    for(let i = i; j<arr.length; j++){
+      sum += arr[j];
+      if(sum === k){
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+// Optimal Approach
+
+function countSubarry(arr, k){
+  let prefixSum = 0;
+  let count = 0;
+  let map = new Map();
+}
