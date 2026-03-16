48e9ca5da754d5a36e49ca8b616d7b984e4e633a
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-16 09:56:01 +0530

    Update 009-heap-sort.js

--- 002-sorting/009-heap-sort.js
+++ 002-sorting/009-heap-sort.js
@@ -1,1 +1,25 @@
+// Heap Sort
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+function heapSort(arr) {
+  let n = arr.length;
 
+  const heapify = (i, size) => {
+    while (true) {
+      let largest = i, l = 2*i + 1, r = 2*i + 2;
+      if (l < size && arr[l] > arr[largest]) largest = l;
+      if (r < size && arr[r] > arr[largest]) largest = r;
+      if (largest === i) break;
+      [arr[i], arr[largest]] = [arr[largest], arr[i]];
+      i = largest;
+    }
+  };
+
+  for (let i = Math.floor(n/2) - 1; i >= 0; i--) heapify(i, n);
+  for (let end = n - 1; end > 0; end--) {
+    [arr[0], arr[end]] = [arr[end], arr[0]];
+    heapify(0, end);
+  }
+
+  return arr;
+}
