f4fe5f4d0b600581144b26f8ff54bf35e4c87371
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-16 16:31:23 +0530

    Update 007-3sum.js

--- 008-two-pointers/code/007-3sum.js
+++ 008-two-pointers/code/007-3sum.js
@@ -72,10 +72,30 @@ function threeSum(nums){
   }
 }
 
+// Optimal Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+function threeSum(nums){
+ nums.sort((a, b) => a - b);
+ const res = [];
+ for(let i = 0; i<nums.length; i++){
+  if(i > 0 && nums[i] === nums[i-1]) continue;
 
+  let left = i+1;
+  let right = nums.length - 1;
 
-
-
-
-
-
+  while(left < right){
+   let sum = nums[i] + nums[left] + nums[right];
+   if(sum === 0){
+    res.push([nums[i], nums[left], nums[right]]);
+    while(nums[left] === nums[left + 1]) left++;
+    while(nums[right] === nums[right - 1]) right--;
+    left++;
+    right--;
+   }
+   else if(sum < 0) left++;
+   else right--;
+  }
+ }
+ return res;
+}
