ca0abbce5d3176cb212a1260730b084b9a55ed3f
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-16 09:52:06 +0530

    Update 004-merge-sort.js

--- 002-sorting/004-merge-sort.js
+++ 002-sorting/004-merge-sort.js
@@ -1,1 +1,52 @@
+// Iterative Merge Sort
+// Time Complexity: O(n log n) [for all: best, average, worst cases]
+// Space Complexity: O(n)
 
+function merge(arr, left, mid, right) {
+  let temp = [];
+
+  let i = left;
+  let j = mid + 1;
+
+  while (i <= mid && j <= right) {
+    if (arr[i] <= arr[j]) {
+      temp.push(arr[i]);
+      i++;
+    } else {
+      temp.push(arr[j]);
+      j++;
+    }
+  }
+
+  while (i <= mid) {
+    temp.push(arr[i]);
+    i++;
+  }
+
+  while (j <= right) {
+    temp.push(arr[j]);
+    j++;
+  }
+
+  for (let k = left; k <= right; k++) {
+    arr[k] = temp[k - left];
+  }
+}
+
+function iterativeMergeSort(arr) {
+  let n = arr.length;
+
+  for (let size = 1; size < n; size *= 2) {
+
+    for (let left = 0; left < n - 1; left += 2 * size) {
+
+      let mid = Math.min(left + size - 1, n - 1);
+
+      let right = Math.min(left + 2 * size - 1, n - 1);
+
+      merge(arr, left, mid, right);
+    }
+  }
+
+  return arr;
+}
