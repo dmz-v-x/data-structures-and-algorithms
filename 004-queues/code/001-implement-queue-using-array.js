45c628a5efa7fe8da3915a20669b145c739e93e3
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-22 11:42:00 +0530

    Create 001-implement-queue-using-array.js

---
+++ 004-queues/code/001-implement-queue-using-array.js
@@ -0,0 +1,13 @@
+// Implement Queue Using Array
+
+// Queue follows FIFO (First In First Out)
+
+// We need to implement
+// enqueue(x)
+// dequeue()
+// peek()
+// isEmpty()
+
+// We will use JavaScript array:
+// Add -> push()
+// Remove -> shift()
