39596dda09d0175fedfa4cd7e1e0a2292592c6ef
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-28 07:08:16 +0530

    Update 009-nth-fibonacci-number.js

--- 012-recursion/code/009-nth-fibonacci-number.js
+++ 012-recursion/code/009-nth-fibonacci-number.js
@@ -18,6 +18,7 @@ console.log(NthFibonacci(10));
 
 // Recursive Approach
 
+// Time Complexity: O(2^n) Not exactly 2^n but nearly exponential
 function NthFibonacci(n){
   if(n <= 1) return n;
   return NthFibonacci(n-1) + NthFibonacci(n - 2);
