494d10f526cd7614a4d82f1265ff1578440b3b60
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-28 07:01:52 +0530

    Create 009-nth-fibonacci-number.js

---
+++ 012-recursion/code/009-nth-fibonacci-number.js
@@ -0,0 +1,32 @@
+// Fibonacci Number
+
+// Iterative Approach
+
+function Nthfibonaaci(n){
+  let f0 = 0;
+  let f1 = 1;
+  let nthFib = 0;
+  for(let i = 2; i<=n; i++){
+    nthFib = f0 + f1;
+    f0 = f1;
+    f1 = nthFib;
+  }
+  return nthFib;
+}
+
+console.log(NthFibonacci(10));
+
+// Recursive Approach
+
+function NthFibonacci(n){
+  if(n <= 1) return n;
+  return NthFibonacci(n-1) + NthFibonacci(n - 2);
+}
+
+
+
+
+
+
+
+
