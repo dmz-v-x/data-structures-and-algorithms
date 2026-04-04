6833ff042ddba0f444f300f0329e1b9c128a68f4
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-04 11:44:34 +0530

    Update 001-implement-stack-using-array.js

--- 003-stacks/code/001-implement-stack-using-array.js
+++ 003-stacks/code/001-implement-stack-using-array.js
@@ -1,3 +1,5 @@
+// Implement Stack using arrays (Fixed Size)
+
 class Stack {
   constructor(size) {
     this.stack = new Array(size);
@@ -61,3 +63,70 @@ stack.push(30);
 console.log(stack.peek()); // 30
 console.log(stack.pop());  // 30
 console.log(stack.size()); // 2
+
+// Time Complexity:
+
+// | Operation | Time |
+// | --------- | ---- |
+// | push      | O(1) |
+// | pop       | O(1) |
+// | peek      | O(1) |
+// | size      | O(1) |
+
+
+
+// Implement Stack using Array (Dynamic Size)
+
+class Stack{
+  constructor(size = 2){
+    this.stack = new Array(size);
+    this.top = -1;
+    this.capacity = size;
+
+    // Resize stack with capacity is full
+
+    resize() {
+      this.capacity = this.capacity * 2;
+      const newStack = new Array(this.capacity);
+
+      for(let i = 0; i<this.top; i++){
+        newStack[i] = stack[i];
+      }
+
+      this.stack = newStack;
+    }
+
+    push(value){
+      if(this.top === this.capacity - 1){
+        this.resize();
+      }
+      this.stack[++this.top] = value;
+    }
+
+    pop(){
+      if(this.top === -1){
+        console.log("Stack Underflow");
+        return null;
+      }
+      return this.stack[this.top--];
+    }
+
+    peek(){
+      if(this.top === -1) return null;
+      return this.stack[this.top];
+    }
+
+    size(){
+      return this.top + 1;
+    }
+
+    print(){
+      console.log(this.stack.slice(0, this.top + 1));
+    }
+  }
+}
+
+// Time Complexity:
+
+// push: worst case: O(n) (when resizing)
+// But overall: O(1) when amortized
