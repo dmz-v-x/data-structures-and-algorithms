d4ea4693149324f91a3c37c6d9577ff440befe6e
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-06 17:07:05 +0530

    Update 002-valid-palindrome.js

--- 008-two-pointers/code/002-valid-palindrome.js
+++ 008-two-pointers/code/002-valid-palindrome.js
@@ -71,3 +71,26 @@ function isPalindrome(s){
   }
   return true;
 }
+
+
+function isPalindrome(s){
+  let start = 0;
+  let end = s.length - 1;
+
+  function isAlphaNum(char){
+    return /[a-z0-9]/i.test(char)
+  }
+
+  while(start < end){
+    while(start < end && !isAlphaNum(s[start])) start++;
+    while(start < end && !isAlphaNum(s[end])) end --;
+
+    if(s[start].toLowerCase() !== s[end].toLowerCase()){
+      return false;
+    }
+
+    start++;
+    end--;
+  }
+  return true;
+}
