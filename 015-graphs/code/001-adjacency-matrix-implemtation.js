c92cd2e34dc84a09f79beb7f7d643f760ef116b2
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-10 10:15:35 +0530

    Update 001-adjacency-matrix-implemtation.js

--- 015-graphs/code/001-adjacency-matrix-implemtation.js
+++ 015-graphs/code/001-adjacency-matrix-implemtation.js
@@ -41,22 +41,63 @@
 // Each inner array represents a row.
 
 // 1. Add Vertex
-Step 1: Push Vertex to Vertices array
-Step 2: Iterate through matrix adding 0 column to each existing row
-Step 3: Creating a new row of size vertices.length and filling it with zero and pushing it to matrix
+// Step 1: Push Vertex to Vertices array
+// Step 2: Iterate through matrix adding 0 column to each existing row
+// Step 3: Creating a new row of size vertices.length and filling it with zero and pushing it to matrix
 
-Matrix always stays square
+// Matrix always stays square
 
-// 2. Getting Vertex Index
-Since matrix Uses numeric Indices we must map vertex name to index
+// // 2. Getting Vertex Index
+// Since matrix Uses numeric Indices we must map vertex name to index
 
-If vertices = ['A', 'B', 'C']
+// If vertices = ['A', 'B', 'C']
 
-A → 0
-B → 1
-C → 2
+// A → 0
+// B → 1
+// C → 2
 
+// // 3. Adding an edge
 
+// In undirected graph if A -- B exists then
+// matrix[A][B] = 1
+// matrix[B][A] = 1
+
+// Step 1: Get the index of vertex v1 and v2
+// Step 2: If vertex indexes v1 or v2 === -1 then vertex does not exists
+// Step 3: this.matrix[i][j] = 1 and this.matrix[j][i] = 1
+
+// // 4. Removing an Edge: Similar to adding an edge
+
+// Step 1: Get the index of vertex v1 and v2
+// Step 2: If vertex indexes v1 or v2 === -1 then vertex does not exists
+// Step 3: this.matrix[i][j] = 0 and this.matrix[j][i] = 0
+
+// // 5. Removing a Vertex
+
+// Step 1: Get the index of vertex if -1 then vertex doesn't exists so return
+// Step 2: Remove vertex using splice
+// Step 3: Remove row using splice
+// Step 4: Remove column from each row again using splice.
+
+// // 6. Making a graph directed
+
+// Step 1: Get the indexes of the vertices, if any one of them is -1 return;
+// Step 2: Then just do this.matrix[i][j] = 1
+
+// // 7. Making a graph weighted
+
+// Step 1: Get the indexes of the vertices, if any one of them is -1 return;
+// Step 2: Then just do this.matrix[i][j] = weight and this.matrix[j][i] = weight
+
+// // 8. Check if edge exists
+
+// Step 1: Get the indexes of the vertexes if any of them is -1 return false
+// Step 2: check: return this.matrix[i][j] !== 0;
+
+// // 9. Printing the Graph
+
+// Step 1: Print Vertices
+// Step2: Print Matrix
 
 class Graph {
   constructor(){
