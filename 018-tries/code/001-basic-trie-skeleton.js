e237153754e5e9225defee58bd67c4b588aa2318
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-13 17:16:03 +0530

    Create 001-basic-trie-skeleton.js

---
+++ 018-tries/code/001-basic-trie-skeleton.js
@@ -0,0 +1,14 @@
+// We combine TrieNode + Trie
+
+class TrieNode {
+  constructor(){
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+class Trie {
+  constructor(){
+    this.root = new TrieNode();
+  }
+}
