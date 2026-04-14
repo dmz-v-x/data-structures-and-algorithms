65c2d1dd61493dbf1401f147fd85c1d75159f3b7
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-14 12:03:40 +0530

    Create 013-n-meeting-in-one-room.js

---
+++ 013-greedy-algorithms/code/013-n-meeting-in-one-room.js
@@ -0,0 +1,66 @@
+// You are given:
+
+// start[] → start times of meetings
+// end[] → end times of meetings
+
+// Only one meeting can happen at a time
+// You must maximize the number of meetings
+
+// Core Idea: Always pick the meeting that ends earliest
+
+// Because:
+
+// It leaves maximum room for future meetings
+
+
+// Steps:
+
+// Step 1: Combine start & end
+  
+// [
+//   {start: 1, end: 2},
+//   {start: 3, end: 4},
+//   ...
+// ]
+
+// Step 2: Sort by end time
+
+// meetings.sort((a, b) => a.end - b.end);
+
+// Step 3: Select meetings
+
+// Pick first meeting
+
+// Then pick next meeting where:
+
+// start > lastEnd
+
+// Time Complexity: O(n logn)
+// Space Complexity: O(n)
+
+function maxMeetings(start, end, n){
+  let meetings = [];
+
+  for(let i = 0; i<n; i++){
+    meetings.push({
+      start: start[i],
+      end: end[i],
+      pos: i+1
+    });
+  }
+
+  meetings.sort((a, b) => b.end - a.end);
+
+  let result = [];
+  let lastEnd = -1;
+
+  for(let meet of meetings){
+    if(meet.start > lastEnd){
+      result.push(meet.pos);
+      lastEnd = meet.end;
+    }
+  }
+  return result;
+}
+
+
