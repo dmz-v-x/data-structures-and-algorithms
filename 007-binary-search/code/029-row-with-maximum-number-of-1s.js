5092c7d7d97710a356d7156bba1e334e9a086d9d
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-16 10:02:13 +0530

    Update 029-row-with-maximum-number-of-1s.js

--- 007-binary-search/code/029-row-with-maximum-number-of-1s.js
+++ 007-binary-search/code/029-row-with-maximum-number-of-1s.js
@@ -24,7 +24,8 @@
 // Explanation: The row indexed 1 has the maximum count of ones (2). So the answer is [1,2].
 
 // Brute Force: Using nested loops
-
+// Time Complexity: O(n * m)
+// Space Complexity: O(1)
 function rowAndMaximumOnes(mat){
   let m = mat.length;
   let n = mat[0].length;
@@ -48,3 +49,96 @@ function rowAndMaximumOnes(mat){
 
 // Better Approach: Binary Search
 
+
+// Idea: 
+
+// Since rows are sorted:
+
+// 0 0 0 1 1 1
+//       ↑ first 1
+
+// Find first occurrence of 1
+// Number of 1s = m - firstIndex
+
+// Better Approach
+// Time complexity: O(n log m);
+// Space Complexity: O(1)
+
+function firstOneIndex(row){
+ let left = 0;
+ let right = row.length - 1;
+ let ans = row.length;
+
+ while(left <= right){
+  let mid = Math.floor((left + right) / 2);
+
+  if(row[mid] === 1){
+   ans = mid;
+   right = mid - 1;
+  }else{
+   left = mid + 1;
+  }
+ }
+ return ans;
+}
+
+function rowWithMax1s(matrix){
+ let maxCount = 0;
+ let index = -1;
+ let m = matrix[0].length;
+
+ for(let i = 0; i<matrix.length; i++){
+  let first = firstOneIndex(matrix[i]);
+  let count = m - first;
+
+  if(count > maxCount){
+   maxCount = count;
+   index = i;
+  }
+ }
+ return index;
+}
+
+// Optimal Approach
+
+// Intuition:
+
+// Start from top-right corner
+
+// Why?
+
+// Left → more 1s possible
+// Down → fewer 1s possible
+
+// Walkthrough
+
+// Start:
+
+// (row = 0, col = m-1)
+
+// Rules:
+
+// If current = 1 → move LEFT (try to find more 1s)
+// If current = 0 → move DOWN
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+function rowWithMax1s(matrix){
+ let n = matrix.length;
+ let m = matrix[0].length;
+
+ let row = 0;
+ let col = m - 1;
+ let index = -1;
+
+ while(row < n && col >= 0){
+  if(matrix[row][col] === 1){
+   index = row;
+   col--; // move left
+  }else{
+   row++;
+  }
+ }
+ return index;
+}
