3ee8a5098877d018f88f2e3d8b4e882783ec2014
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-16 17:12:25 +0530

    Update 006-preorder-inorder-postorder-in-one-traversal.js

--- 014-binary-tree/code/006-preorder-inorder-postorder-in-one-traversal.js
+++ 014-binary-tree/code/006-preorder-inorder-postorder-in-one-traversal.js
@@ -1,1 +1,42 @@
+// We want to get:
+
+
+// Preorder → Root → Left → Right
+// Inorder → Left → Root → Right
+// Postorder → Left → Right → Root
+
+// Core Idea:
+
+// Instead of visiting a node once…
+
+// We visit each node 3 times using a stack
+
+// And we track:
+
+// (node, state)
+
+function allTravesal(root){
+  const pre = [];
+  const inorder = [];
+  const post = [];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
