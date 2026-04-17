588331afa6f1f3a3f40faa247177a12fed19a7b2
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-17 15:30:21 +0530

    Update 019-determine-if-two-events-have-conflicts.js

--- 013-greedy-algorithms/code/019-determine-if-two-events-have-conflicts.js
+++ 013-greedy-algorithms/code/019-determine-if-two-events-have-conflicts.js
@@ -1,1 +1,21 @@
+// You are given two events (intervals):
 
+// event1 = [s1, e1]
+// event2 = [s2, e2]
+
+// You need to check:
+
+// Do they overlap (conflict)?
+
+// Intuition
+
+// Case 1: Conflict
+// [1------5]
+//      [3------7]
+
+// There exists time (3,4,5) where both are active → conflict
+
+// Case 2: No Conflict
+// [1------2]    [3------5]
+
+// No common time → no conflict
