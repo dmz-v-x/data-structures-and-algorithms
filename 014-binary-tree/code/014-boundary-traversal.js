62bff9a51075260b1132669564892beb8d9dc245
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-20 18:19:41 +0530

    Create 014-boundary-traversal.js

---
+++ 014-binary-tree/code/014-boundary-traversal.js
@@ -0,0 +1,15 @@
+Boundary traversal means:
+
+Traverse the OUTER boundary of the tree in anti-clockwise direction
+
+Example
+          1
+        /   \
+       2     3
+      / \     \
+     4   5     6
+        / \   /
+       7   8 9
+
+Output
+[1, 2, 4, 7, 8, 9, 6, 3]
