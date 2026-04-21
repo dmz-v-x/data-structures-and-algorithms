cb9698c751cc88e0fc9e6aa96ec20dd6f79290f9
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-21 21:22:32 +0530

    Create 026-print-all-nodes-at-distance-k-in-binary-tree.js

---
+++ 014-binary-tree/code/026-print-all-nodes-at-distance-k-in-binary-tree.js
@@ -0,0 +1,23 @@
+// Given:
+
+// a binary tree
+// a target node
+// an integer k
+
+// Return all nodes exactly k distance away from target
+
+// Distance = number of edges
+
+// Example
+//         3
+//        / \
+//       5   1
+//      / \ / \
+//     6  2 0  8
+//       / \
+//      7   4
+
+// target = 5, k = 2
+
+// Answer:
+// [7, 4, 1]
