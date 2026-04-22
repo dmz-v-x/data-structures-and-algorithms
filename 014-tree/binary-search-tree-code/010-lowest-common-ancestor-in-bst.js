50632975e09a994fdfe8565a975622052d0ce4a7
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-22 21:03:18 +0530

    Create 010-lowest-common-ancestor-in-bst.js

---
+++ 014-tree/binary-search-tree-code/010-lowest-common-ancestor-in-bst.js
@@ -0,0 +1,29 @@
+// WHAT IS LCA?
+
+// LCA of two nodes p and q is:
+
+// The lowest node where both p and q are in its subtree
+
+// Example
+//         6
+//        / \
+//       2   8
+//      / \ / \
+//     0  4 7  9
+//       / \
+//      3   5
+
+// LCA of 2 and 8:
+// Answer = 6
+
+// LCA of 3 and 5:
+// Answer = 4
+
+// WHY BST MAKES THIS EASY
+
+// In normal binary tree → complex
+// In BST → super simple because
+
+// LEFT < ROOT < RIGHT
+
+CORE IDEA
