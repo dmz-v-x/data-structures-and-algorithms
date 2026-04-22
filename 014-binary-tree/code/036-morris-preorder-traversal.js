1a3318e3c7882c7ad6715790b12412fd6815be6f
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-22 14:15:11 +0530

    Create 036-morris-preorder-traversal.js

---
+++ 014-binary-tree/code/036-morris-preorder-traversal.js
@@ -0,0 +1,18 @@
+// Morris Preorder Traversal
+
+// Normal traversals:
+
+// | Method    | Space |
+// | --------- | ----- |
+// | Recursion | O(H)  |
+// | Stack     | O(H)  |
+
+// Morris:
+
+// O(1) space
+
+// CORE IDEA
+
+// Instead of stack/recursion:
+
+// We temporarily modify the tree (threads)
