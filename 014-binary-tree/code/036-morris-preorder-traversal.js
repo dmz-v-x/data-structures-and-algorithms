7f68bc5ce02dfb17650f9afbb9e08d9bad558245
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-22 14:44:14 +0530

    Update 036-morris-preorder-traversal.js

--- 014-binary-tree/code/036-morris-preorder-traversal.js
+++ 014-binary-tree/code/036-morris-preorder-traversal.js
@@ -16,3 +16,53 @@
 // Instead of stack/recursion:
 
 // We temporarily modify the tree (threads)
+
+// WHAT IS A "THREAD"?
+
+// We connect:
+
+// Rightmost node of left subtree → current node
+
+// PREORDER REMINDER
+// ROOT → LEFT → RIGHT
+
+// So we must:
+
+// Print node BEFORE going left
+
+
+function morrisPreorder(root) {
+  let curr = root;
+  let result = [];
+
+  while (curr) {
+
+    // Case 1: no left child
+    if (!curr.left) {
+      result.push(curr.val);
+      curr = curr.right;
+    } 
+    else {
+      // find predecessor
+      let pred = curr.left;
+
+      while (pred.right && pred.right !== curr) {
+        pred = pred.right;
+      }
+
+      // thread not created
+      if (!pred.right) {
+        result.push(curr.val);   // preorder print
+        pred.right = curr;       // create thread
+        curr = curr.left;
+      } 
+      else {
+        // thread exists → remove
+        pred.right = null;
+        curr = curr.right;
+      }
+    }
+  }
+
+  return result;
+}
