0433c107bfa3c56cdfba570b608e46d5e8c14ea9
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-23 09:14:12 +0530

    Update 010-lowest-common-ancestor-in-bst.js

--- 014-tree/binary-search-tree-code/010-lowest-common-ancestor-in-bst.js
+++ 014-tree/binary-search-tree-code/010-lowest-common-ancestor-in-bst.js
@@ -26,4 +26,91 @@
 
 // LEFT < ROOT < RIGHT
 
-CORE IDEA
+// CORE IDEA
+
+// At any node:
+
+// Case 1:
+// p and q BOTH < root
+
+// Go LEFT
+
+// Case 2:
+// p and q BOTH > root
+
+// Go RIGHT
+
+// Case 3:
+// p and q are on different sides
+
+// THIS is LCA 
+
+// Example: p = 2, q = 8
+// At 6:
+// 2 < 6 and 8 > 6
+// → split happens → LCA = 6
+
+// Example: p = 3, q = 5
+// At 6 → go left
+// At 2 → go right
+// At 4:
+// 3 < 4 and 5 > 4 → split → LCA = 4
+
+// Recursive
+
+function lowestCommonAncestor(root, p, q){
+  if(!root) return null;
+
+  if(p.val < root.val && q.val < root.val){
+    return lowestCommonAncestor(root.left, p, q);
+  }
+
+  if(p.val > root.val && q.val > root.val){
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root; // split point
+}
+
+
+// Iterative Solution
+
+function lowestCommonAncestor(root, p, q){
+  let curr = root;
+  while(curr){
+    if(p.val < curr.val && q.val < curr.val){
+      curr = curr.val
+    }else if(p.val > curr.val && q.val > curr.val){
+      curr = curr.right;
+    }else{
+      return curr;
+    }
+  }
+}
+
+
+// Complexity
+// Balanced BST
+// Time = O(log N)
+// Worst case
+// Time = O(N)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
