b6c0232969386b24a72dacd0c767ecf6c2d01f35
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-05-05 11:43:12 +0530

    Update 066-count-number-of-good-subarrays.js

--- 001-arrays/code/066-count-number-of-good-subarrays.js
+++ 001-arrays/code/066-count-number-of-good-subarrays.js
@@ -19,23 +19,39 @@
 // - [1,4,3,2,2,4] that has 2 pairs.
 // - [4,3,2,2,4] that has 2 pairs.
 
+// Complexity
+// Time: O(n)
+// Space: O(n)
 
+function countGood(nums, k){
+  let n = nums.length;
+  let map = new Map();
 
+  let left = 0;
+  let pairs = 0;
+  let count = 0;
 
+  for(let right = 0; right<n; right++){
+    let val = nums[right];
 
+    // existing frequency contributes to new pairs
+    let freq = map.get(val) || 0;
+    pairs += freq;
 
+    map.set(val, freq+1);
 
+    while(pairs >= k){
+      count += (n - right);
 
+      let leftVal = nums[left];
+      let leftFreq = map.get(left);
 
-
-
-
-
-
-
-
-
-
+      pairs -= (leftFreq - 1);
+      left++;
+    }
+  }
+  return count;
+}
 
 
 
