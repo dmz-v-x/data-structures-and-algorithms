3b02e9e1070c87d1a0fd35ad6bf03e3559e09932
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-05-06 10:47:32 +0530

    Update 015-two-sum.js

--- 001-arrays/code/015-two-sum.js
+++ 001-arrays/code/015-two-sum.js
@@ -37,16 +37,14 @@ function twoSum(nums, target){
 // Time complexity: O(n)
 // Space Complexity: O(n)
 function twoSum(nums, target){
-  let map = new Map(); // numbet -> index
-
-  for(let i = 0; i<nums.length; i++){
-    if(map.has(target - nums[i])){
-      return [i, map.get(target, nums[i])];
+  let map = new Map();
+    for(let i = 0; i<nums.length; i++){
+      if(map.has(target - nums[i])){
+        return [i, map.get(target - nums[i])];
+      }else{
+        map.set(nums[i], i);
+      }
     }
-
-    map.set(nums[i], i);
-  }
-  return [];
 }
 
 
