5b260e05977b3b2d8551f97f98838b664b8668dc
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-05-07 16:16:32 +0530

    Update 018-longest-consecutive-sequence.js

--- 011-hashing/code/018-longest-consecutive-sequence.js
+++ 011-hashing/code/018-longest-consecutive-sequence.js
@@ -98,7 +98,7 @@ function longestConsecutive(nums){
 
   let longest = 0;
 
-  for(let num of nums){
+  for(let num of set){
     if(!set.has(num - 1)){
       let current = num;
       let length = 1;
