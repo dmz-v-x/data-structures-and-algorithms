5668725a1012c840b51fba54afae1a05f5286ebd
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-05-07 09:59:14 +0530

    Update 012-binary-array-with-sum-goal.js

--- 008-two-pointers/code/012-binary-array-with-sum-goal.js
+++ 008-two-pointers/code/012-binary-array-with-sum-goal.js
@@ -50,14 +50,123 @@ function numSubarrayWithSum(nums, goal){
   return count;
 }
 
-// Better Approach
+// Better Approach Prefix Sum + HashMap
 
-“Before starting, we have seen a prefix sum of 0 exactly once.”
+// “Before starting, we have seen a prefix sum of 0 exactly once.”
 
+// Dry Run
 
+// nums = [1,0,1,0,1]
+// goal = 2
 
+// Initialize:
 
+// map = {
+//   0: 1
+// }
 
+// Why?
+
+// Because:
+
+// prefix sum 0 already exists before array starts
+
+// Step 1
+// num = 1
+// sum = 1
+
+// need = sum - goal
+//      = 1 - 2
+//      = -1
+
+// Not found.
+
+// Store:
+
+// map = {
+//   0:1,
+//   1:1
+// }
+
+// Step 2
+// num = 0
+// sum = 1
+
+// need = 1 - 2 = -1
+
+// Not found.
+
+// Update:
+
+// map[1] = 2
+
+// Step 3
+// num = 1
+// sum = 2
+
+// need = 2 - 2 = 0
+
+// map[0] = 1
+
+// So:
+
+// count += 1
+
+// One valid subarray found.
+
+// Step 4
+// num = 0
+// sum = 2
+
+// need = 0
+
+// Again:
+
+// count += map[0]
+//        += 1
+// Step 5
+// num = 1
+// sum = 3
+
+// need = 1
+
+// map[1] = 2
+
+// Meaning:
+
+// 2 previous prefix sums had value 1
+
+// So:
+
+// count += 2
+
+// Final:
+
+// count = 4
+
+function numSubarraysWithSum(nums, goal) {
+  let map = new Map();
+
+  map.set(0, 1);
+
+  let sum = 0;
+  let count = 0;
+
+  for (let num of nums) {
+
+    sum += num;
+
+    let need = sum - goal;
+
+    if (map.has(need)) {
+      count += map.get(need);
+    }
+
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+
+  return count;
+}
 
 
 
