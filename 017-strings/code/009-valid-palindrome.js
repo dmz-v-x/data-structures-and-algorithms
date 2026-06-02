9bc128d820c9f85a50379e46545a0af3116b1305
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-06-02 11:33:23 +0530

    Update 009-valid-palindrome.js

--- 017-strings/code/009-valid-palindrome.js
+++ 017-strings/code/009-valid-palindrome.js
@@ -37,11 +37,11 @@ function isValidPalindrome(s){
   let left = 0;
   let right = s.length - 1;
   while(left < right){
-    while(left < right && !/[^a-z0-9]/i.test(s[left])){
+    while(left < right && /[^a-z0-9]/i.test(s[left])){
       left++;
     }
 
-    while(left < right && !/[^a-z0-9]/i.test(s[right])){
+    while(left < right && /[^a-z0-9]/i.test(s[right])){
       right--;
     }
 
